61568531bd33a56ba9eaa678bfb9269046df12ef
Marlowe Recht <recht.marlowe@gmail.com>
2023-05-15 12:58:05 -0400

    updated albums table and tested seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# ALBUMS
+puts "seeding albums..."
+taylor_swift_debut = Album.create!(title = "Taylor Swift", release_date = 2006, runtime: "53:29", cover_art: "https://upload.wikimedia.org/wikipedia/en/1/1f/Taylor_Swift_-_Taylor_Swift.png")
